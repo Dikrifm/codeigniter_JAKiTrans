4768d1839caff3848d1064f6d4636979057fae45
Dikri <jakitrans.id@gmail.com>
2023-02-01 11:00:44 +0700

    #A1 error 30

--- application/controllers/api/Pelanggan.php
+++ application/controllers/api/Pelanggan.php
@@ -3036,7 +3036,7 @@ function update_saldo_post()
             'code'   => 200,
             'message'=> 'Success',
             'status' => true,
-            'data'   => 'test'
+            'data'   => $cekdata
         );
 
         $this->response($message, 200);
