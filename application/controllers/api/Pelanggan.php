ea1a5c8aa1b2a94bec6a4f7286c900f382e43c2e
Dikri <jakitrans.id@gmail.com>
2023-02-02 09:57:45 +0700

    #A1 error 44

--- application/controllers/api/Pelanggan.php
+++ application/controllers/api/Pelanggan.php
@@ -3046,8 +3046,8 @@ function update_saldo_post()
             }
 
             $arr[] = [
-                "role_r" => $role_r//,
-                //$body => $this->Pelanggan_model->get_transaksi_saldo_by_r($decoded_data->id, $init_r)
+                "role_r" => $role_r,
+                "body"   => $this->Pelanggan_model->get_transaksi_saldo_by_r($decoded_data->id, $init_r)
             ];
             
         }
