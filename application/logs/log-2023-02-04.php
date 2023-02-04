f678e716b8b88ee2c93b2651a3557d3ab734ee44
Dikri <jakitrans.id@gmail.com>
2023-02-04 16:09:07 +0700

    gitignore 14

---
+++ application/logs/log-2023-02-04.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2023-02-04 07:55:36 --> Severity: error --> Exception: Access denied for user 'jakitran_ojol'@'localhost' (using password: YES) E:\GitHub\codeigniter_JAKiTrans\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2023-02-04 07:55:49 --> Severity: error --> Exception: Access denied for user 'jakitran_ojol'@'localhost' (using password: YES) E:\GitHub\codeigniter_JAKiTrans\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2023-02-04 07:55:51 --> Severity: error --> Exception: Access denied for user 'jakitran_ojol'@'localhost' (using password: YES) E:\GitHub\codeigniter_JAKiTrans\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2023-02-04 07:57:58 --> Severity: error --> Exception: Access denied for user 'jakitran_ojol'@'localhost' (using password: YES) E:\GitHub\codeigniter_JAKiTrans\system\database\drivers\mysqli\mysqli_driver.php 203
