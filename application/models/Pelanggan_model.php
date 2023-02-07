517e91fa35ada1cf18cc49887a290cdc7d1a5979
Dikri <jakitrans.id@gmail.com>
2023-02-07 10:55:09 +0700

    #A1 56

--- application/models/Pelanggan_model.php
+++ application/models/Pelanggan_model.php
@@ -2788,9 +2788,9 @@ public function merchantnearby($long, $lat)
 
             //VALIDASI SENDER
             if($init_s == "P"){
-                //$cond_id = array('id' => $q['sender_user_id']);
-                //$query_s = $this->get_data_pelanggan($cond_id)->result();
-                $name_s  = "cust_s";//$query_s->fullnama;
+                $cond_id = array('id' => $q['sender_user_id']);
+                $query_s = $this->get_data_pelanggan($cond_id)->result();
+                $name_s  = $query_s->fullnama;
 
             }elseif($init_s == "D"){
                 //$cond_id = array('id' => $q['sender_user_id']);
