57a3c5b55cc4d8cb7dc5c98a4936dc34b62abe1f
Dikri <jakitrans.id@gmail.com>
2023-02-09 10:06:15 +0700

    #QR 17

--- application/views/payment/zoom_qr.php
+++ application/views/payment/zoom_qr.php
@@ -3,6 +3,12 @@
         <div class="card-body">
 
             <img src="<?= base_url('images/qr/'.$image_path)?>" alt="QR_CODE_Image">
+            <h4>Nama Event   : <?= $nama_event ?></h4>
+            <h4>Nominal      : <?= $nominal ?></h4>
+            <h4>Tipe         : <?= $tipe ?></h4>
+            <h4>Status       : <?= $status ?></h4>
+            <h4>create date  : <?= $qrstring ?></h4>
+            <h4>expired date : <?= $nama_event ?></h4>
 
         </div><!-- /.CARD-Body -->
     </div><!-- /.CARD-->
