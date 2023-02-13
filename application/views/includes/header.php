613d0100ab249e052ebd0ac1860afbe6f6a4a984
Dikri <jakitrans.id@gmail.com>
2023-02-13 11:12:57 +0700

    #QR 25 10

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -132,13 +132,14 @@ License: You must have a valid license purchased only from themeforest to legall
             </ul>
 
 		</nav>
+        <!--
         <form role="search" class="navbar-search">
             <div class="position-relative">
                 <a href="javascript:void(0);" class="navbar-search-icon"><span class="feather-icon"><i data-feather="search"></i></span></a>
-                <!--<input type="text" name="example-input1-group2" class="form-control" placeholder="Ketik ID Pesanan Mu Disini ...">-->
+                <input type="text" name="example-input1-group2" class="form-control" placeholder="Ketik ID Pesanan Mu Disini ...">
                 <a id="navbar_search_close" class="navbar-search-close" href="#"><span class="feather-icon"><i data-feather="x"></i></span></a>
             </div>
-        </form>
+        </form>-->
         <!-- /Top Navbar -->
 
         <!-- Vertical Nav -->
