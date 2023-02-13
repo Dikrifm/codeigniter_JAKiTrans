77dbdf6a62daf85e35d75fad836c445e9cec2141
Dikri <jakitrans.id@gmail.com>
2023-02-13 10:20:00 +0700

    #QR 25 6

--- application/controllers/Payments.php
+++ application/controllers/Payments.php
@@ -387,7 +387,7 @@ class Payments extends CI_Controller
     function qr_download($id){
         $data_qr = $this->payment->get_qr_event_by_id($id);
 
-        $image_file = base_url('images/qr/'. 'qr-230702011021.png');//$data_qr['image_path']);
+        $image_file = base_url('images/qr/'. $data_qr['image_path']);
 
         return force_download($image_file, NULL);
     }
