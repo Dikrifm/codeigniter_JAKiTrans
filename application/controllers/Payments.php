bb0e7738e327460172287325fb46777e2ca7476a
Dikri <jakitrans.id@gmail.com>
2023-02-13 14:03:56 +0700

    #QR 28 5

--- application/controllers/Payments.php
+++ application/controllers/Payments.php
@@ -386,6 +386,43 @@ class Payments extends CI_Controller
     }
     
     function qrcode(){
+        /*
+        $qr_url1 = FCPATH.'images/qr/testqr1.png';
+        $qr_url2 = FCPATH.'images/qr/testqr2.png';
+        $logo_url = FCPATH.'images/logo.png';
+
+        $params['data'] = 'cek123'; 
+        $params['level'] = 'H'; //H=High
+        $params['size'] = 10;
+        $params['savename'] = $qr_url1;
+        
+        $this->Ciqrcode->generate($params);
+
+        $QR = imagecreatefrompng($qr_url1);
+
+        memulai menggambar logo dalam file qrcode
+        $logo = imagecreatefromstring(file_get_contents($logo_url));
+        
+        imagecolortransparent($logo , imagecolorallocatealpha($logo , 0, 0, 0, 127));
+        imagealphablending($logo , false);
+        imagesavealpha($logo , true);
+        
+        $QR_width = imagesx($QR);//get logo width
+        $QR_height = imagesy($QR);//get logo width
+
+        $logo_width = imagesx($logo);
+        $logo_height = imagesy($logo);
+        Scale logo to fit in the QR Code
+        $logo_qr_width = $QR_width/2;
+        $scale = $logo_width/$logo_qr_width;
+        $logo_qr_height = $logo_height/$scale;
+
+        imagecopyresampled($QR, $logo, $QR_width/4, $QR_height/3.5, 0, 0, $logo_qr_width, $logo_qr_height, $logo_width, $logo_height);
+        
+        Simpan kode QR lagi, dengan logo di atasnya
+        imagepng($QR,$qr_url);
+        */
+        
         /* 
         param 
         (1)qrcontent,
@@ -397,18 +434,19 @@ class Payments extends CI_Controller
         (7)forecolor,
         (8)backcolor 
         */
-
+        
         QRcode::png(
             "cekcek",
-            base_url('images/test1.png'),
+            'images/test123.png',
             "H",
             20, 
             4,
             0,
             "0,255,0",
             "255,255,255",
-            false
+            //base_url('images/logo.png')
         );
+        
     }
     
     function reportqr(){
