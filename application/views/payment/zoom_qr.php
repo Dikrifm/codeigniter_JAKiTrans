eff0f988c17960167f6bc3a83fa3b8485a06cd95
Dikri <jakitrans.id@gmail.com>
2023-02-14 13:48:45 +0700

    29 6

--- application/views/payment/zoom_qr.php
+++ application/views/payment/zoom_qr.php
@@ -37,7 +37,7 @@
                 Print QR : <?= substr($nama_event, 0, 15).' . . .' ?>
                 </button>
             </a>
-            <a href="<?= base_url('images/no_image.png')?>" class="btn btn-outline-secondary">
+            <a href="<?= base_url('images/qr/'.$image_path)?>" class="btn btn-outline-secondary">
             Download QR
             </a>
         </div><!-- /.CARD-Body -->
