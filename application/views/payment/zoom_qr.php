80127723c34d27c1f0f8861064b80da07a853bb5
Dikri <jakitrans.id@gmail.com>
2023-02-14 14:00:05 +0700

    29 9

--- application/views/payment/zoom_qr.php
+++ application/views/payment/zoom_qr.php
@@ -12,34 +12,46 @@
         <?php   
             }
         ?>  
-            
-            <h4>Nama Event   : <?= $nama_event ?></h4>
-            <h4>Nominal      : <?= $nominal ?></h4>
-            <h4>Tipe         : <?= $tipe ?></h4>
-            <?php 
-            if($status == 1){
-                $status_qr = 'Active';
-                $color     = 'success';
-            }else{
-                $status_qr = 'non-Active';
-                $color     = 'danger';
-            } 
-            ?>
-            <h4>
-                Status       : <span class="badge badge-<?= $color ?>">
-                               <?= $status_qr ?>
-                               </span>
-            </h4>
-            <h4>created date : <?= $created_date ?></h4>
-            <h4>expired date : <?= $expired_date ?></h4>
-            <a>
-                <button class='btn btn-outline-secondary' onclick='window.print()' >
-                Print QR : <?= substr($nama_event, 0, 15).' . . .' ?>
-                </button>
-            </a>
-            <a href="<?= base_url('images/qr/'.$image_path)?>" class="btn btn-outline-secondary" download="QR : <?= $nama_event ?>">
-            Download QR
-            </a>
+
+        <table>
+            <tr>
+                <th>Nama Event</th>
+                <th>Nominal</th>
+                <th>Tipe</th>
+                <th>Status</th>
+                <th>Created Date</th>
+                <th>Expired Date</th>
+            </tr>
+            <tr>
+                <td><h4><?= $nama_event ?></h4>
+                <td><h4><?= $nominal ?></h4></td>
+                <td><h4><?= $tipe ?></h4></td>
+                <?php 
+                if($status == 1){
+                    $status_qr = 'Active';
+                    $color     = 'success';
+                }else{
+                    $status_qr = 'non-Active';
+                    $color     = 'danger';
+                } 
+                ?>
+                <td><h4>
+                    Status       : <span class="badge badge-<?= $color ?>">
+                                <?= $status_qr ?>
+                                </span>
+                </h4></td>
+                <td><h4>created date : <?= $created_date ?></h4></td>
+                <td><h4>expired date : <?= $expired_date ?></h4></td>
+            </tr>
+        </table>
+                <a>
+                    <button class='btn btn-outline-secondary' onclick='window.print()' >
+                    Print QR : <?= substr($nama_event, 0, 15).' . . .' ?>
+                    </button>
+                </a>
+                <a href="<?= base_url('images/qr/'.$image_path)?>" class="btn btn-outline-secondary" download="QR - <?= $nama_event ?>">
+                Download QR
+                </a>
         </div><!-- /.CARD-Body -->
     </div><!-- /.CARD-->
 </div><!-- /.Content-wrapper
