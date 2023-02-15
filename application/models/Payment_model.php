c174c945c887ef7e70f7a670f466376643ce6a57
Dikri <jakitrans.id@gmail.com>
2023-02-15 10:24:14 +0700

    32 3

--- application/models/Payment_model.php
+++ application/models/Payment_model.php
@@ -1035,7 +1035,7 @@ class Payment_model extends CI_model
         );
         */
         $data_saldo = array(
-            'id_user' => 'test1',
+            'id_user' => $data['id'],
             'saldo'   => 0
         );
 
