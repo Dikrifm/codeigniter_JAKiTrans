9b7d40a8c8366350d06cd9071baf05d070f1fd54
Dikri <jakitrans.id@gmail.com>
2023-02-15 16:59:02 +0700

    36 6

--- application/views/payment/zoom_qr.php
+++ application/views/payment/zoom_qr.php
@@ -65,41 +65,39 @@
                 Download QR
                 </a>
         
-        <div class='card'>
-            <div class='card-body'>
-                <h4>History QR : <?=$nama_event?> Total saldo : Rp. <?=number_format($saldo_qr,0,',','.')?></h4>
-                <table class="table" id="detail_qr">
-                <thead>
-                <tr>
-                    <th>No</th>                    
-                    <th>Invoice</th>
-                    <th>Date Time</th>
-                    <th>Nominal</th>
-                    <th>ID User</th>
-                    <th>Nama User</th>
-                </tr>
-                </thead>
-                <tbody>
-                <?php 
-                    $i = 1;
-                    foreach($detail_qr as $d){       
-                ?>
-                <tr>
-                    <td><?=$i?></td>
-                    <td><?=$d['invoice_w']?></td>
-                    <td><?=$d['regtime']?></td>
-                    <td><?=number_format($d['jumlah_w'],0,',','.')?></td>
-                    <td><?=$d['id_user_w']?></td>
-                    <td><?=$d['nama_user']?></td>
-                <tr>
-                <?php
-                    $i++;
-                    }
-                ?>
-                </tbody>
-                </table>
-            </div>
-        </div>
+        
+        <h4>History QR : <?=$nama_event?> Total saldo : Rp. <?=number_format($saldo_qr,0,',','.')?></h4>
+        <table class="table table-striped table-hover dt-responsive display nowrap" id="detail_qr">
+        <thead>
+            <tr>
+                <th>No</th>                    
+                <th>Invoice</th>
+                <th>Date Time</th>
+                <th>Nominal</th>
+                <th>ID User</th>
+                <th>Nama User</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php 
+                $i = 1;
+                foreach($detail_qr as $d){       
+            ?>
+            <tr>
+                <td><?=$i?></td>
+                <td><?=$d['invoice_w']?></td>
+                <td><?=$d['regtime']?></td>
+                <td><?=number_format($d['jumlah_w'],0,',','.')?></td>
+                <td><?=$d['id_user_w']?></td>
+                <td><?=$d['nama_user']?></td>
+            <tr>
+            <?php
+                $i++;
+                }
+            ?>
+        </tbody>
+        </table>
+            
 
         </div><!-- /.CARD-Body -->
     </div><!-- /.CARD-->
