00bce6eed8640ffdac5b7028ae2b4016e659370d
Dikri <jakitrans.id@gmail.com>
2023-03-01 11:36:39 +0700

    #

---
+++ application/logs/log-2023-03-01.php
@@ -0,0 +1,5 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2023-03-01 05:34:56 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): No connection could be made because the target machine actively refused it.
+ E:\GitHub\ci\codeigniter_JAKiTrans\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2023-03-01 05:34:56 --> Unable to connect to the database
